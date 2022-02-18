e4fe1c04e93abb4a3eb5abc26fa5b41490be7e1a
francomac <franco.mcarballo@outlook.com>
2022-02-18 16:38:13 -0600

    add flushPromises and UT for EventList

--- tests/unit/EventList.spec.js
+++ tests/unit/EventList.spec.js
@@ -1,84 +1,93 @@
 import Vuex from 'vuex'
 import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+
 import EventList from '@/views/EventList.vue'
+import EventCard from '@/components/EventCard.vue'
+import BaseIcon from '@/components/BaseIcon'
+
+import { events as mockEvents } from '../../db.json'
+import flushPromises from 'flush-promises'
+
+const localVueWithVuex = createLocalVue()
+localVueWithVuex.use(Vuex)
+
+function mountEventList() {
+  let eventActions = {
+    fetchEvents: jest.fn()
+  }
+
+  let user = {
+    namespaced: true,
+    state: {
+      user: {
+        name: 'franco'
+      }
+    }
+  }
+  let event = {
+    namespaced: true,
+    state: {
+      events: mockEvents
+    },
+    actions: eventActions
+  }
+
+  let storeInstance = new Vuex.Store({
+    modules: {
+      user,
+      event
+    }
+  })
+
+  return mount(EventList, {
+    localVue: localVueWithVuex,
+    store: storeInstance,
+    stubs: {
+      RouterLink: RouterLinkStub,
+      EventCard,
+      BaseIcon
+    },
+    mocks: {
+      $route: { query: { page: 2 } }
+    }
+  })
+}
 
 let wrapper
 
 beforeEach(() => {
-  const localVue = createLocalVue()
-
-  localVue.use(Vuex)
-  const mockStore = new Vuex.Store({
-    modules: {
-      user: {
-        namespaced: true,
-        state: {
-          user: { name: 'alice' }
-        }
-      },
-      event: {
-        namespaced: true,
-        events: {
-          id: 1,
-          title: 'Beach Cleanup',
-          date: 'Aug 28 2018',
-          time: '10:00',
-          location: 'Daytona Beach',
-          description: "Let's clean up this beach.",
-          organizer: 'Adam Jahr',
-          category: 'sustainability',
-          attendees: [
-            {
-              id: 'abc123',
-              name: 'Adam Jahr'
-            },
-            {
-              id: 'def456',
-              name: 'Gregg Pollack'
-            },
-            {
-              id: 'ghi789',
-              name: 'Beth Swanson'
-            },
-            {
-              id: 'jkl101',
-              name: 'Mary Gordon'
-            }
-          ]
-        },
-        actions: {
-          fetchEvents: jest.fn()
-        }
-      }
-    }
-  })
-
-  const myEventCard = {
-    template: '<div />',
-    methods: {
-      someMethod() {}
-    }
-  }
-
-  wrapper = mount(EventList, {
-    localVue,
-    stubs: {
-      RouterLink: RouterLinkStub,
-      EventCard: myEventCard
-    },
-    mocks: {
-      $store: mockStore,
-      $route: { query: { page: 2 } }
-    }
-  })
+  jest.resetAllMocks()
 })
 
 describe('EventList', () => {
-  it('should displays a specific title', () => {
-    const name = 'alice'
-    const title = wrapper.find("[data-testid=eventListTitle]")
-    expect(title.exists()).toBeTruthy()
+  it('should render the events list component', () => {
+    wrapper = mountEventList()
+    expect(wrapper.exists()).toBeTruthy()
+  })
 
-    expect(title.text()).toEqual(`Events for ${name}`)
+  describe('page title', () => {
+    it('should displays a specific text', () => {
+      wrapper = mountEventList()
+      const name = 'franco'
+      const title = wrapper.find('[data-testid=eventListTitle]')
+
+      expect(title.exists()).toBeTruthy()
+
+      expect(title.text()).toEqual(`Events for ${name}`)
+    })
+  })
+
+  describe('events', () => {
+    it('are rendered in a list', async () => {
+      wrapper = mountEventList()
+
+      await flushPromises()
+      
+      console.log(wrapper.vm.$store._modules.root.state.event)
+
+      expect(wrapper.findAllComponents(EventCard).length).toBe(
+        mockEvents.length
+      )
+    })
   })
 })
